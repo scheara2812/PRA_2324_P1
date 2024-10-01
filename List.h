6705b48edad788f6926489ac01a7afd24775f890
Saúl Chen <scheara@etsinf.upv.es>
2024-10-01 13:06:25 +0200

    Interfaz List

--- List.h
+++ List.h
@@ -4,7 +4,14 @@
 template <typename T> 
 class List {
     public:
-        // ... aquí los métodos virtuales puros
+	    void insert(int pos, T e);
+	    void append(T e);
+	    void prepend(T e);
+	    T remove(int pos);
+	    T get(int pos);
+	    int search(T e);
+	    bool empty();
+	    int size();
 };
 
 #endif
